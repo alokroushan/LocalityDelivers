6a5fde830874321ba453aaebed1edc11893788a2
Alok Roushan <236236773+alokroushan@users.noreply.github.com>
2026-01-10 21:32:45 +0530

    Update API_KEY in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
+    'process.env.API_KEY': JSON.stringify("AIzaSyDnl1hq6UX8OQxzDFJs8Ft24NZ3amQJmkU")
   },
   server: {
     port: 3000
