1882738bd648ed5a727a7b218186fe92e62b8269
Alok Roushan <236236773+alokroushan@users.noreply.github.com>
2026-01-11 17:08:24 +0530

    Remove API_KEY from vite.config.ts
    Removed API_KEY definition from Vite config.

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'process.env.API_KEY': JSON.stringify("AIzaSyDI6Lg-lFqpU6yERUAmJh8OfOCR-b3mR8s")
-  },
   server: {
     port: 3000
   }
